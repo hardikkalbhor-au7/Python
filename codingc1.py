af27ac895b925855739e1b59c84c2e64a8eccd4c
hardik0602 <hardik0602@github.com>
2020-01-28 20:37:27 +0530

    first commit

---
+++ codingc1.py
@@ -0,0 +1,15 @@
+#coding challenge
+#1.  
+
+#2. 1 mb=8000000 b   1 Gb= 8589934592 b
+
+#3.program for 5 subjects marks
+
+Sub1=int(input("Enter marks for 1: "))
+Sub2=int(input("Enter marks for 2: "))
+Sub3=int(input("Enter marks for 3: "))
+Sub4=int(input("Enter marks for 4: "))
+Sub5=int(input("Enter marks for 5: "))
+
+perc=int(Sub1+Sub2+Sub3+Sub4+Sub5)/5
+print(perc)
